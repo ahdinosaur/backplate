f5c3e1323123807140cd0b17224daf6d0d9a1ec2
Michael Williams <michael.williams@enspiral.com>
2020-06-26 13:30:48 +1200

    add more tests
    turns out nested templates work out-of-the-box !

--- test/mod.test.ts
+++ test/mod.test.ts
@@ -37,3 +37,27 @@ test('render out-of-order keys', () => {
   const expectedString = 'a b c!'
   expect(actualString).toBe(expectedString)
 })
+
+test('render nested data', () => {
+  const source = 'hello ${a.b.c}!'
+  const data = { a: { b: { c: 'earth' } } }
+  const actualString = render(source, data)
+  const expectedString = 'hello earth!'
+  expect(actualString).toBe(expectedString)
+})
+
+test('render array map', () => {
+  const source = 'hello ${names.join(", ")}!'
+  const data = { names: ['alice', 'bob'] }
+  const actualString = render(source, data)
+  const expectedString = 'hello alice, bob!'
+  expect(actualString).toBe(expectedString)
+})
+
+test('render nested template', () => {
+  const source = 'hello ${names.map(name => `${name} friend`).join(", ")}!'
+  const data = { names: ['alice', 'bob'] }
+  const actualString = render(source, data)
+  const expectedString = 'hello alice friend, bob friend!'
+  expect(actualString).toBe(expectedString)
+})
